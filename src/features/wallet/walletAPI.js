105fce09e3bd2b3e492972445c90b7f0d0fac241
Alex Yusypenko <o.yusipenko@gmail.com>
2021-08-15 14:00:18 +0300

    fix

--- src/features/wallet/walletAPI.js
+++ src/features/wallet/walletAPI.js
@@ -1,3 +1,5 @@
+import HmacSHA256 from "crypto-js/hmac-sha256";
+
 function getCoinPrice(coinName) {
   const response = fetch(
     `https://www.binance.com/api/v3/ticker/price?symbol=${coinName}BUSD`
@@ -18,8 +20,32 @@ export function fetchCoinsPrice(coins) {
   return response;
 }
 
-export function checkCoin(coinName) {
-  const response = getCoinPrice(coinName);
-
+export async function checkCoin(coinName) {
+  const response = await getCoinPrice(coinName);
+  console.log(response);
+  console.log(!!response);
   return !!response;
+}
+
+export function testHistoryOrders() {
+  const ts = new Date().getTime();
+  const apiKey =
+    "HURrvrfCi7lw6HMmMCmmiahVteMJOih5oKvoltBYuMpFdnzLQvMSA0wyNAPzBhJL";
+  console.log(ts);
+  console.log(HmacSHA256);
+  const hashDigest = HmacSHA256(
+    apiKey,
+    "Xrnzga2Km9uuCAUEs3QTZK2IWbnL2KdJncRQqhwDnVzLe8suu1kTDnoJVjK2cfmJ"
+  );
+  console.log(hashDigest);
+  const response = fetch(
+    `https://www.binance.com/api/v3/allOrders?symbol=BTCBUSD&timestamp=${ts}`,
+    { headers: { "X-MBX-APIKEY": `${hashDigest}` } }
+  )
+    .then((response) => response.json())
+    .catch(function (e) {
+      console.log("error", e);
+    });
+
+  return response;
 }
